e555827c71b2191c216ce4c676b5c5a464e1c22b
Sumit kumar <78586294+nothing-available@users.noreply.github.com>
2023-07-15 16:08:49 +0530

    Update three.js

--- iterations/three.js
+++ iterations/three.js
@@ -1,1 +1,37 @@
+// for of
+
+// ["","",""]
+// [{},{},{},{}]
+
+const arr = [1,2,3,4,5]
+//syntax
+// for(consy interator of object){
+    
+// }
+
+for(const num of arr){
+    console.log(num);
+}
+
+const greetings = "hello Sumit"
+for(const greet of greetings){
+    console.log(`Each char is ${greet}`)
+}
+
+// Maps
+
+const map = new Map()
+map.set('IN', "india")
+map.set('USA', "America")
+map.set('Fr', "France")
+map.set('IN', "india")
+
+console.log(map)
+
+for(const [key,value] of map){
+    console.log(key,'-',value)
+}
+
+
+
 
